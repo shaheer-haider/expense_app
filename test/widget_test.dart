1c8bb5a5b58cf60a534ee4bd2da9593dfcfa9b47
shaheer-haider <56408067+shaheer-haider@users.noreply.github.com>
2021-09-07 00:19:36 +0500

    Flutter moved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:expense_app/main.dart';
+import 'package:the_expense_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
